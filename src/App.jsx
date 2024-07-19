9f2df2d8d5cb238bdaabab5e7843eb348eee7c97
Karishma <dube.karishmaa@gmail.com>
2024-07-19 15:14:52 +0530

    tailwind sample project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='card rounded-none'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+      {/* <section className='h-screen bg-twitter-blue m-12 flex justify-center items-center text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-18'>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+           It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </section> */}
+    </div>
+  )
+}
+
+export default App
